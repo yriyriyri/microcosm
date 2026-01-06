c2ee7a6a2c8e9fbf0b2f266e07f3e480288b3805
yriyriyri <oscar.paterson93@gmail.com>
2026-01-07 05:45:37 +0900

    try

--- src/components/VoxelEditor/VoxelWorldEditor.tsx
+++ src/components/VoxelEditor/VoxelWorldEditor.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+//test
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
